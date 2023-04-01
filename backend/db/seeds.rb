73a83d05c271f01f3ed1149579c145b404e27a31
NexoCasion <NathanZMacedo@gmail.com>
2023-04-01 09:28:12 -0300

    seed criada

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Travel.create(  
+    nome:Faker::Travel::Airport.name(size: 'large', region: 'united_states'),
+    desc:Faker::Address.city,
+    data:Faker::Date.between(from: '2023-09-23', to: '2023-09-25'),
+    price:Faker::Number.decimal(l_digits: 2),
+)
+
+puts Travel.last
