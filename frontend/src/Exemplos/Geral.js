462bfe7d4376eb2a67b2afa73fd536058e3b6e1a
NexoCasion <nathanzmacedo@gmail.com>
2023-04-15 09:19:26 -0300

    props children

--- frontend/src/Exemplos/Geral.js
+++ frontend/src/Exemplos/Geral.js
@@ -1,10 +1,17 @@
 import FunctionSimples from "./componentes/FunctionSimples";
 import FunctionDupla from "./componentes/FunctionDupla";
+import Botao from "./componentes/layout/Botao";
+
+
 function Geral(){
 return (<>
+
     <FunctionSimples/>
-    <FunctionDupla/>
-</>
-)
+    <FunctionDupla/>    
+    <Botao>Entrar</Botao>
+    <Botao>Sair</Botao>
+    </>
+    )
 }
+
 export default Geral;
---
+++ frontend/src/Exemplos/componentes/layout/Botao.js
@@ -0,0 +1,6 @@
+function Botao(props){
+    return(
+        <button>{props.children}</button>
+    )
+}
+export default Botao
