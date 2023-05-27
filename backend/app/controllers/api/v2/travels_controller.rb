68e31bb62ead5a24a260b4e880a85da238870cd7
NexoCasion <nathanzmacedo@gmail.com>
2023-05-27 09:25:28 -0300

    clonando controler v1 pro v2

--- backend/app/controllers/api/v2/travels_controller.rb
+++ backend/app/controllers/api/v2/travels_controller.rb
@@ -1,16 +1,39 @@
 class Api::V2::TravelsController < ApplicationController
   def index
+    @travels = Travel.all
+    render json: @travels
+  
   end
 
   def show
   end
 
-  def update
+  def create
+    @travel = Travel.new(travel_params)
+    if @travel.save
+      render json: @travel
+    else 
+      render json:{errors: @travel.errors,message: "deu ruim pra cadastrar"},status: :unprocessable_entity
+    end
   end
 
-  def create
+  def update
+    @travel = Travel.find(params[:id])
+    if @travel.update(travel_params)
+      render json:@travel
+    else
+      render json:{errors: @travel.errors,message: "deu ruim para editar"},status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @travel = Travel.find(params[:id])
+    @travel.destroy
+    render json: {message: "Sua Viagem foi deletada com  sucesso!"}
+  end
+
+  private
+  def travel_params
+    params.require(:travel).permit(:nome,:data,:price,:desc)
   end
 end
